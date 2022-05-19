4d71b2a7bf1f505e427bab97ef4c92bf4e22f9e3
Antoine Martin <antoine@mrtn.fr>
2022-05-19 18:32:07 +0200

    🎉 Initial import

---
+++ README.md
@@ -0,0 +1,209 @@
+# PowerShell-Wsl-Arch
+
+Powershell cmdlet to quickly create a small Arch Linux based WSL distribution.
+It is available in PowerShell Gallery as
+[`Wsl-Arch`](https://www.powershellgallery.com/packages/Wsl-Arch/1.1.1).
+
+## Rationale
+
+As a developer working mainly on Linux, I have the tendency to put everything in
+the same WSL distribution, ending up with a distribution containing Go, Python,
+Terraform...
+
+This module is here to reduce the cost of spawning a new distrbution for
+development in order to have concerns more splitted. It is also a reminder of
+the congfiguration steps to have a working distribution.
+
+## What it does
+
+This module provides a cmdlet called `Install-WslArch` that will install a
+lightweight Windows Subsystem for Linux (WSL) distribution based on Arch Linux
+Linux.
+
+This command performs the following operations:
+
+- Create a Distribution directory,
+- Download the Root Filesystem,
+- Create the WSL distribution,
+- Configure the WSL distribution.
+
+The distribution is configured as follows:
+
+- A user named `alpine` is set as the default user. The user as `doas` (BSD
+  version of sudo used in Arch Linux) privileges.
+- zsh with [oh-my-zsh](https://ohmyz.sh/) is used as shell.
+- [powerlevel10k](https://github.com/romkatv/powerlevel10k) is set as the
+  default oh-my-zsh theme.
+- [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions) plugin
+  is installed.
+- The
+  [wsl2-ssh-pageant](https://github.com/antoinemartin/wsl2-ssh-pageant-oh-my-zsh-plugin)
+  plugin is installed in orther to use the GPG keys private keys available at
+  the Windows level (I'm using a Yubikey).
+
+## Pre-requisites
+
+WSL 2 needs to be installed and working. If you are on Windows 11, a simple
+`wsl --install` should get you going.
+
+To install this module, you need to be started with the
+[PowerShell Gallery](https://docs.microsoft.com/en-us/powershell/scripting/gallery/getting-started?view=powershell-7.2).
+
+The WSL distribution uses a fancy zsh theme called
+[powerlevel10k](https://github.com/romkatv/powerlevel10k). To work properly in
+the default configuration, you need a [Nerd Font](https://www.nerdfonts.com/).
+My personal advice is to use `Ubuntu Mono NF` available via [scoop](scoop.sh) in
+the nerds font bucket:
+
+```console
+> scoop bucket add nerd-fonts
+> scoop install UbuntuMono-NF-Mono
+```
+
+The font name is then `'UbuntuMono NF'` (for vscode, Windows Terminal...).
+
+## Getting started
+
+Install the module with:
+
+```console
+> Install-Module -Name Wsl-Arch
+```
+
+And then create a WSL distribution with:
+
+```console
+> Install-WslArch
+####> Creating directory [C:\Users\AntoineMartin\AppData\Local\WslArch]...
+####> Downloading https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/x86_64/alpine-minirootfs-3.15.0-x86_64.tar.gz â†’ C:\Users\AntoineMartin\AppData\Local\WslArch\rootfs.tar.gz...
+####> Creating distribution [WslArch]...
+####> Running initialization script on distribution [WslArch]...
+####> Done. Command to enter distribution: wsl -d WslArch
+>
+```
+
+You can specify the name of the distribution:
+
+```console
+> Install-WslArch alpine2
+...
+```
+
+To uninstall the distribution, just type:
+
+```console
+> Uninstall-WslArch alpine2
+>
+```
+
+It will remove the distrbution and wipe the directory completely.
+
+## Example: Creating a distribution hosting docker
+
+You can create a distribution for building docker images. Fist install the
+distribution:
+
+```powershell
+❯ install-WslArch docker
+####> Creating directory [C:\Users\AntoineMartin\AppData\Local\docker]...
+####> Downloading https://dl-cdn.alpinelinux.org/alpine/v3.15/releases/x86_64/alpine-minirootfs-3.15.0-x86_64.tar.gz â†’ C:\Users\AntoineMartin\AppData\Local\docker\rootfs.tar.gz...
+####> Creating distribution [docker]...
+####> Running initialization script on distribution [docker]...
+####> Done. Command to enter distribution: wsl -d docker
+❯
+```
+
+Then connect to it as root and install docker:
+
+```bash
+# Connect to the distribution
+❯ wsl -d docker -u root
+[powerlevel10k] fetching gitstatusd .. [ok]
+# Add docker
+❯ apk --update add docker
+(1/13) Installing libseccomp (2.5.2-r0)
+...
+OK: 304 MiB in 103 packages
+# Enabling OpenRC
+❯ openrc default
+ * Caching service dependencies ...         [ ok ]
+# Start docker with OpenRC
+❯ rc-update add docker default
+ * service docker added to runlevel default
+# Start OpenRC, and hence docker, on distribution startup
+❯ cat >/etc/wsl.conf <<EOF
+heredoc> [boot]
+heredoc> command = /sbin/openrc default
+heredoc> EOF
+# Allow default user to run docker
+❯ addgroup alpine docker
+# Return to powershell
+❯ exit
+# Terminate distrbution
+❯ wsl --terminate docker
+# Start distribution and docker
+❯ wsl -d docker docker ps
+CONTAINER ID   IMAGE     COMMAND   CREATED   STATUS    PORTS     NAMES
+```
+
+Now, with this distribution, you can add the following alias to
+`%USERPROFILE%\Documents\WindowsPowerShell\profile.ps1`:
+
+```powershell
+function RunDockerInWsl {
+  wsl -d docker /usr/bin/docker $args
+}
+Set-Alias -Name docker -Value RunDockerInWsl
+```
+
+and run docker directly from powershell:
+
+```powershell
+❯ docker run --rm -it alpine:latest /bin/sh
+Unable to find image 'alpine:latest' locally
+latest: Pulling from library/alpine
+df9b9388f04a: Pull complete
+Digest: sha256:4edbd2beb5f78b1014028f4fbb99f3237d9561100b6881aabbf5acce2c4f9454
+Status: Downloaded newer image for alpine:latest
+/ #
+```
+
+You can save the distrbution root filesystem for reuse:
+
+```powershell
+❯ Export-WslArch docker -OutputFile $env:USERPROFILE\Downloads\docker.tar.gz
+Distribution docker saved to C:\Users\AntoineMartin\Downloads\docker.tar.gz
+```
+
+And then recreate the distribution in the same state from the exported root
+filesystem:
+
+```powershell
+❯ Uninstall-WslArch docker
+❯ Install-WslArch docker -SkipConfigure -RootFSURL file://$env:USERPROFILE\Downloads\docker.tar.gz
+####> Creating directory [C:\Users\AntoineMartin\AppData\Local\docker]...
+####> Downloading file://C:\Users\AntoineMartin\Downloads\docker.tar.gz â†’ C:\Users\AntoineMartin\AppData\Local\docker\rootfs.tar.gz...
+####> Creating distribution [docker]...
+####> Done. Command to enter distribution: wsl -d docker
+❯ wsl -d docker docker ps
+CONTAINER ID   IMAGE     COMMAND   CREATED   STATUS    PORTS     NAMES
+❯
+```
+
+## Development
+
+To modify the module, clone it in your local modules directory:
+
+```console
+> cd $env:USERPROFILE\Documents\WindowsPowerShell\Modules\
+> git clone https://github.com/antoinemartin/PowerShell-Wsl-Arch Wsl-Arch
+```
+
+## TODO
+
+- [x] Add a switch to avoid the configuration of the distribution.
+- [x] Document the customization of the distrbution.
+- [x] Add a command to export the current filesystem and use it as input for
+      other distrbutions.
+- [ ] Allow publication of the module through github actions.
+- [ ] Publish the customized root filesystem to improve startup.
---
+++ Wsl-Arch.psm1
@@ -0,0 +1,320 @@
+# Copyright 2022 Antoine Martin
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+if ($IsWindows) {
+    $wslPath = "$env:windir\system32\wsl.exe"
+    if (-not [System.Environment]::Is64BitProcess) {
+        # Allow launching WSL from 32 bit powershell
+        $wslPath = "$env:windir\sysnative\wsl.exe"
+    }
+
+}
+else {
+    # If running inside WSL, rely on wsl.exe being in the path.
+    $wslPath = "wsl.exe"
+}
+
+
+$module_directory = ([System.IO.FileInfo]$MyInvocation.MyCommand.Path).DirectoryName
+
+
+function Install-WslArch {
+    <#
+    .SYNOPSIS
+        Installs and configure a minimal Arch Linux based WSL distribution.
+
+    .DESCRIPTION
+        This command performs the following operations:
+        - Create a Distribution directory
+        - Download the Root Filesystem.
+        - Create the WSL distribution.
+        - Configure the WSL distribution.
+
+        The distribution is configured as follow:
+        - A user named `arch` is set as the default user.
+        - zsh with oh-my-zsh is used as shell.
+        - `powerlevel10k` is set as the default oh-my-zsh theme.
+        - `zsh-autosuggestions` plugin is installed.
+
+    .PARAMETER DistributionName
+        The name of the distribution. If ommitted, will take WslArch by
+        default.
+
+    .PARAMETER RootFSURL
+        URL of the root filesystem. By default, it will take the official 
+        Arch Linux root filesystem.
+
+    .PARAMETER BaseDirectory
+        Base directory where to create the distribution directory. Equals to 
+        $env:APPLOCALDATA (~\AppData\Local) by default.
+
+    .PARAMETER SkipConfigure
+        Skip Configuration.
+
+    .INPUTS
+        None.
+
+    .OUTPUTS
+        None.
+
+    .EXAMPLE
+        Install-WslArch toto
+    
+    .LINK
+        Uninstall-WslArch
+        https://github.com/romkatv/powerlevel10k
+        https://github.com/zsh-users/zsh-autosuggestions
+        https://github.com/antoinemartin/wsl2-ssh-pageant-oh-my-zsh-plugin
+
+    .NOTES
+        The command tries to be indempotent. It means that it will try not to
+        do an operation that already has been done before.
+
+    #>    
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Position = 0)]
+        [string]$DistributionName = "WslArch",
+        [string]$RootFSURL = "https://github.com/yuk7/ArchWSL-FS/releases/download/22051900/rootfs.tar.gz",
+        [string]$BaseDirectory = $env:LOCALAPPDATA,
+        [Parameter(Mandatory = $false)]
+        [switch]$SkipConfigure
+    )
+
+
+    # Where to install the distribution
+    $distribution_dir = "$BaseDirectory\$DistributionName"
+
+    # Create the directory
+    If (!(test-path $distribution_dir)) {
+        Write-Host "####> Creating directory [$distribution_dir]..."
+        $null = New-Item -ItemType Directory -Force -Path $distribution_dir
+    }
+    else {
+        Write-Host "####> Distribution directory [$distribution_dir] already exists."
+    }
+
+    $rootfs_file = "$distribution_dir\rootfs.tar.gz"
+
+    # Donwload the root filesystem
+    If (!(test-path $rootfs_file)) {
+        Write-Host "####> Downloading $RootFSURL → $rootfs_file..."
+        if ($PSCmdlet.ShouldProcess($rootfs_file, 'Download root fs')) {
+            (New-Object Net.WebClient).DownloadFile($RootFSURL, $rootfs_file)
+        }
+    }
+    else {
+        Write-Host "####> Root FS already at [$rootfs_file]."
+    }
+
+    # Retrieve the distribution if it already exists
+    $current_distribution = Get-ChildItem HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss |  Where-Object { $_.GetValue('DistributionName') -eq $DistributionName }
+
+    If ($null -eq $current_distribution) {
+        Write-Host "####> Creating distribution [$DistributionName]..."
+        if ($PSCmdlet.ShouldProcess($DistributionName, 'Create distribution')) {
+            &$wslPath --import $DistributionName $distribution_dir $rootfs_file | Write-Verbose
+        }
+    }
+    else {
+        Write-Host "####> Distribution [$DistributionName] already exists."
+    }
+
+    if ($false -eq $SkipConfigure) {
+        Write-Host "####> Running initialization script on distribution [$DistributionName]..."
+        if ($PSCmdlet.ShouldProcess($DistributionName, 'Configure distribution')) {
+            Copy-Item -Path "$module_directory\.p10k.zsh" -Destination "\\wsl$\$DistributionName\tmp\.p10k.zsh"
+            Copy-Item -Path "$module_directory\configure.sh" -Destination "\\wsl$\$DistributionName\tmp\configure.sh"
+            &$wslPath -d $DistributionName -u root /bin/sh /tmp/configure.sh 2>&1 | Write-Verbose
+    
+            Get-ChildItem HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss |  Where-Object { $_.GetValue('DistributionName') -eq $DistributionName } | Set-ItemProperty -Name DefaultUid -Value 1000
+        }    
+    }
+
+
+    Write-Host "####> Done. Command to enter distribution: wsl -d $DistributionName"
+    ## More Stuff ?
+    # To import your publick keys and use the yubikey for signing.
+    #  gpg --keyserver keys.openpgp.org --search antoine@mrtn.fr
+}
+
+function Uninstall-WslArch {
+    <#
+    .SYNOPSIS
+        Uninstalls Arch Linux based WSL distribution.
+
+    .DESCRIPTION
+        This command unregisters the specified distribution. It also deletes the
+        distribution base root filesystem and the directory of the distribution.
+
+    .PARAMETER DistributionName
+        The name of the distribution. If ommitted, will take WslArch by
+        default.
+
+    .PARAMETER BaseDirectory
+        Base directory where to create the distribution directory. Equals to 
+        $env:APPLOCALDATA (~\AppData\Local) by default.
+    
+    .PARAMETER KeepDirectory
+        If specified, keep the distribution directory. This allows recreating
+        the distribution from a saved root file system.
+
+    .INPUTS
+        None.
+
+    .OUTPUTS
+        None.
+
+    .EXAMPLE
+        Uninstall-WslArch toto
+    
+    .LINK
+        Install-WslArch
+        https://github.com/romkatv/powerlevel10k
+        https://github.com/zsh-users/zsh-autosuggestions
+        https://github.com/antoinemartin/wsl2-ssh-pageant-oh-my-zsh-plugin
+
+    .NOTES
+        The command tries to be indempotent. It means that it will try not to
+        do an operation that already has been done before.
+
+    #>    
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Position = 0)]
+        [string]$DistributionName = "WslArch",
+        [string]$BaseDirectory = $env:LOCALAPPDATA,
+        [Parameter(Mandatory = $false)]
+        [switch]$KeepDirectory
+    )
+
+    # Retrieve the distribution if it already exists
+    $current_distribution = Get-ChildItem HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss |  Where-Object { $_.GetValue('DistributionName') -eq $DistributionName }
+
+    # Where to install the distribution
+    $distribution_dir = "$BaseDirectory\$DistributionName"
+
+    if ($null -eq $current_distribution) {
+        Write-Error "Distribution $DistributionName doesn't exist !" -ErrorAction Stop
+    }
+    else {
+        if ($PSCmdlet.ShouldProcess($DistributionName, 'Unregister distribution')) {
+            Write-Verbose "Unregistering WSL distribution $DistributionName"
+            &$wslPath --unregister $DistributionName 2>&1 | Write-Verbose 
+        }
+        if ($false -eq $KeepDirectory) {
+            Remove-Item -Path $distribution_dir -Recurse
+        }
+    }
+
+}
+
+function Export-WslArch {
+    <#
+    .SYNOPSIS
+        Exports the file system of an Arch Linux WSL distrubtion.
+
+    .DESCRIPTION
+        This command exports the distribution and tries to compress it with 
+        the `gzip` command embedded in the distribution. If no destination file
+        is given, it replaces the root filesystem file in the distribution 
+        directory.
+
+    .PARAMETER DistributionName
+        The name of the distribution. If ommitted, will take WslArch by
+        default.
+
+    .PARAMETER BaseDirectory
+        Base directory where to create the distribution directory. Equals to 
+        $env:APPLOCALDATA (~\AppData\Local) by default.
+    
+    .PARAMETER OutputFile
+        The name of the output file. If it is not specified, it will overwrite
+        the root file system of the distribution.
+
+    .INPUTS
+        None.
+
+    .OUTPUTS
+        None.
+
+    .EXAMPLE
+        Install-WslArch toto
+        wsl -d toto -u root apk add openrc docker
+        Export-WslArch toto
+
+        Uninstall-WslArch toto -KeepDirectory
+        Install-WslArch toto -SkipConfigure
+    
+    .LINK
+        Install-WslArch
+        https://github.com/romkatv/powerlevel10k
+        https://github.com/zsh-users/zsh-autosuggestions
+        https://github.com/antoinemartin/wsl2-ssh-pageant-oh-my-zsh-plugin
+
+    .NOTES
+        The command tries to be indempotent. It means that it will try not to
+        do an operation that already has been done before.
+
+    #>    
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Position = 0)]
+        [string]$DistributionName = "WslArch",
+        [string]$BaseDirectory = $env:LOCALAPPDATA,
+        [Parameter(Mandatory = $false)]
+        [string]$OutputFile
+    )
+
+    # Retrieve the distribution if it already exists
+    $current_distribution = Get-ChildItem HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss |  Where-Object { $_.GetValue('DistributionName') -eq $DistributionName }
+
+    # Where to install the distribution
+    $distribution_dir = "$BaseDirectory\$DistributionName"
+
+    if ($null -eq $current_distribution) {
+        Write-Error "Distribution $DistributionName doesn't exist !" -ErrorAction Stop
+    }
+    else {
+        if ($PSCmdlet.ShouldProcess($DistributionName, 'Export distribution')) {
+            if ("" -eq $OutputFile) {
+                $out_file = "$distribution_dir\rootfs.tar.gz"
+            }
+            else {
+                $out_file = "$OutputFile"
+            }
+            $export_file = "$distribution_dir\export.tar"
+
+            Write-Verbose "Exporting WSL distribution $DistributionName to $export_file"
+            &$wslPath --export $DistributionName "$export_file" | Write-Verbose 
+            $filepath = (Get-Item -Path "$export_file").Directory.FullName
+            Write-Verbose "Compressing export.tar in $filepath"
+            Remove-Item "$export_file.gz" -Force -ErrorAction SilentlyContinue
+            &$wslPath -d $DistributionName --cd "$filepath" gzip export.tar | Write-Verbose
+
+            If (test-path "$out_file") {
+                Remove-Item "$out_file"
+            }
+            Write-Verbose "Renaming $export_file.gz to $out_file"
+            Move-Item -Path "$export_file.gz" "$out_file"
+            Write-Host "Distribution $DistributionName saved to $out_file"
+        }
+    }
+
+}
+
+Export-ModuleMember Install-WslArch
+Export-ModuleMember Uninstall-WslArch
+Export-ModuleMember Export-WslArch
